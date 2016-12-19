2a2869d25572e29a211ff7e72557591fff241675
Stefan Hartmann <stefanhartmann@gmail.com>
2016-12-19 12:44:25 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use("/", express.static(__dirname + '/build'));
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log('petinder server now online!');
+});
+
+app.use(function(err, req, res, next){
+  res.status(err.status || 500);
+});
